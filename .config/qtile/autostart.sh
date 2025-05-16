2e2837cf2f3100d5e932ca899d9f1ece459542fb
Craig Chambers <cchambers@s6c.mlp.college>
2025-05-16 09:22:48 +0100

    Added NM-Applet as autostart application

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,7 +2,8 @@
 nm-applet &
 
 # background
-feh --bg-scale /usr/share/backgrounds/Monument_valley_by_orbitelambda.jpg &
+# feh --bg-scale /usr/share/backgrounds/Monument_valley_by_orbitelambda.jpg &
+nitrogen --restore &
 
 # compositor
 picom --config ~/.config/picom/picom.conf &
@@ -12,3 +13,6 @@ sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
 # Notifications
 dunst &
+
+# Network Manager
+nm-applet &
