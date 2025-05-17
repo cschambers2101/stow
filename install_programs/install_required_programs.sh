80021c44b0efa440294d0fd73fe428905ed3e8f6
Craig Chambers <Craig Chambers>
2025-05-17 06:35:39 +0100

    Changed script to install packages in one command and added qtile install script

--- install_programs/install_required_programs.sh
+++ install_programs/install_required_programs.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.local/bambu ~/.local/share/backgrounds ~/.local/share/ca-certificate
 
 # Check if the package list file exists
 if [ ! -f "programs_to_install.txt" ]; then
-    echo "Error: installed.txt not found!"
+    echo "Error: programs_to_install.txt not found!"
     exit 1
 fi
 
@@ -15,21 +15,46 @@ fi
 echo "Updating package list..."
 sudo apt-get update
 
-# Read each line from installed.txt and install the package
-echo "Installing packages from installed.txt..."
-while IFS= read -r package || [[ -n "$package" ]]; do
-    # Skip empty lines or lines starting with # (comments)
-    if [[ -z "$package" || "$package" =~ ^# ]]; then
-        continue
+# Read packages from the file into an array
+readarray -t packages < "programs_to_install.txt"
+
+# Filter out empty lines and comments
+packages=("${packages[@]//$'\r'/}") # Remove carriage returns (CR)
+packages=("${packages[@]//#[^\r]*/}") # Remove comments
+packages=("${packages[@]//^[ \t]*\r?$/}") # Remove empty lines
+
+# Check if there are any packages to install
+if [ ${#packages[@]} -gt 0 ]; then
+    echo "Attempting to install the following packages: ${packages[*]}"
+    # Install the packages with a single apt install command
+    sudo apt-get install -y -o Acquire::http::Pipeline-Depth="0" "${packages[@]}"
+
+    if [ $? -eq 0 ]; then
+        echo "Successfully installed all specified packages."
+    else
+        echo "Error occurred during package installation."
     fi
-    echo "Attempting to install $package..."
-    sudo apt-get install -y -o Acquire::http::Pipeline-Depth="0" "$package"
-    if [ $? -ne 0 ]; then
-        echo "Warning: Failed to install $package. It might not exist or there was an error."
-    fi
-done < "programs_to_install.txt"
+else
+    echo "No packages to install."
+fi
 
 sudo apt autoremove -y
+
+
+# Install Qtile
+script_to_run="install_qtile.sh"
+
+if [ -e "$script_to_run" ]; then
+  echo "Script '$script_to_run' exists. Executing..."
+  bash "$script_to_run"
+  if [ $? -eq 0 ]; then
+    echo "Script '$script_to_run' executed successfully."
+  else
+    echo "Script '$script_to_run' failed with exit code $?."
+  fi
+else
+  echo "Error: Script '$script_to_run' does not exist. Continuing with the main script."
+fi
 
 echo "Script finished."
 exit 0
