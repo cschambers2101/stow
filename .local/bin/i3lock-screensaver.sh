eb1bbc83846f323ece179ab8ce850ec164d7fc20
Craig Chambers <cchambers@s6c.mlp.college>
2026-01-13 09:15:54 +0000

    Paused dunst nitifications on screen lock and added .local/bin to path via .bash_profile

--- .local/bin/i3lock-screensaver.sh
+++ .local/bin/i3lock-screensaver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script to capture all screens, blur them, and use as the i3lock background.
 
+# Pause Dunst notifications
+dunstctl set-paused true
+
 # 1. Take a screenshot of the entire desktop (all monitors)
 scrot /tmp/lock_screen.png
 
@@ -10,7 +13,10 @@ convert /tmp/lock_screen.png -scale 10% -scale 1000% /tmp/lock_screen_pixel.png
 
 # 3. Lock the screen using the prepared image
 #    The -i flag is used here, but it points to the generated image.
-i3lock -i /tmp/lock_screen_pixel.png
+i3lock -n -i /tmp/lock_screen_pixel.png
 
 # 4. Clean up
 rm /tmp/lock_screen.png /tmp/lock_screen_pixel.png
+
+# 5. Resume Dunst notifications
+dunstctl set-paused false
