0a4879a98d9157ea3c889ffa456400d39b7ceb0c
Craig Chambers <cchambers@s6c.mlp.college>
2026-03-19 14:14:00 +0000

    Added screenlock after 3 minutes with xss-lock

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -22,10 +22,16 @@ nm-applet &
 # Set keymap
 setxkbmap -layout gb
 
-# Kill any potentially running screen locker/screensaver
-# killall xscreensaver &
-#
-# # Start the xscreensaver daemon (silently)
-# xscreensaver -no-splash &
-#
+# 1. Kill any existing instances to avoid duplicates
+pkill xss-lock
+
+# 2. Set the idle timeout to 600 seconds (10 mins)
+# We also disable 'blanking' so your script handles the visuals immediately
+xset s 180 180
+xset s noblank
+xset dpms 180 180 180
+
+# 3. Start the listener with the correct filename
+xss-lock --transfer-sleep-lock -- /bin/bash /home/craigchambers/.local/bin/i3lock-screensaver.sh &
+
 xrandr --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --left-of DP-2 --rotate normal
